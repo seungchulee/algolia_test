6961e4c8da006c753247a8d21929513c5ed76288
SC <inthetelevision@gmail.com>
2023-03-24 11:33:57 +0900

    feat: basic search test

--- main.go
+++ main.go
@@ -1,21 +1,24 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/algolia/algoliasearch-client-go/v3/algolia/opt"
 	"github.com/algolia/algoliasearch-client-go/v3/algolia/search"
 )
 
-type Record struct {
-	ObjectID string `json:"objectID"`
-	Name     string `json:"name"`
-}
-
 func main() {
-	client := search.NewClient("", "")
+	client := search.NewClient("HKWXA0EXKH", "")
 
-	index := client.InitIndex("")
-	resSave, err := index.SaveObjects(Record{ObjectID: "", Name: ""})
+	index := client.InitIndex("test_datium")
+	params := []interface{}{
+		opt.AttributesToRetrieve("email", "company", "city"),
+		opt.HitsPerPage(1),
+	}
+	res, err := index.Search("Kathleen", params...)
 	if err != nil {
 		panic(err)
 	}
-	resSave.Wait()
+	fmt.Println(res.Hits[0])
+
 }

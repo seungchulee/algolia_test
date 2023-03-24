6d7cca75be65f0046050744bf9d0a9c9d4843809
SC <inthetelevision@gmail.com>
2023-03-24 11:54:19 +0900

    feat: add env

--- main.go
+++ main.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/algolia/algoliasearch-client-go/v3/algolia/opt"
 	"github.com/algolia/algoliasearch-client-go/v3/algolia/search"
 )
 
+type Record struct {
+	ObjectID string `json:"objectID"`
+	Name     string `json:"name"`
+}
+
 func main() {
-	client := search.NewClient("HKWXA0EXKH", "")
+	apiKey := os.Getenv("key")
+	client := search.NewClient("HKWXA0EXKH", apiKey)
 
 	index := client.InitIndex("test_datium")
+	// write
+	resSave, err := index.SaveObject(Record{ObjectID: "test", Name: "test"})
+	if err != nil {
+		panic(err)
+	}
+	resSave.Wait()
+
+	// search
 	params := []interface{}{
 		opt.AttributesToRetrieve("email", "company", "city"),
 		opt.HitsPerPage(1),
@@ -20,5 +35,4 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(res.Hits[0])
-
 }
